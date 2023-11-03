97a6b70f9f9f7fefb7321ce1eb9849806aeaef2a
Erik Karlsson <erikdkarlsson8@gmail.com>
2023-11-02 22:52:07 -0700

    Added scripts dir and some more ui init files. Going to download extra log files.

---
+++ scripts/list-eq-files.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+#
+ll | grep -v -E '\.zon|:crc|\.eqg|\.s3d|\.mp3|\.emt|\.dll|\.eff|\.pfs|\.txt|\.wav'
+ll | grep -v -E '\.zon|:crc|\.eqg|\.s3d|\.mp3|\.emt|\.dll|\.eff|\.pfs|\.txt|\.wav|\.xmi|\.ico|\.edd|\.m3d|\.pak|\.dg0|\.eal|\.asi|\.flt'
