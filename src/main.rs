47a30560fa3b4096bed238b8d52800a233009a26
Erik Karlsson <erikdkarlsson8@gmail.com>
2023-11-08 17:11:28 -0800

    Merging remote in

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,23 @@
+use rdev::{EventType,Key};
 mod macros;
 mod events;
 mod server;
 
 fn main() {
-    server::serve::start_server();
+    // server::serve::start_server();
+    events::keyboard::send(&EventType::KeyPress(Key::KeyS));
+    events::keyboard::send(&EventType::KeyRelease(Key::KeyS));
+    events::keyboard::send(&EventType::KeyPress(Key::Space));
+    events::keyboard::send(&EventType::KeyRelease(Key::Space));
+
+    events::keyboard::send(&EventType::KeyPress(Key::KeyS));
+    events::keyboard::send(&EventType::KeyRelease(Key::KeyS));
+    events::keyboard::send(&EventType::KeyPress(Key::Space));
+    events::keyboard::send(&EventType::KeyRelease(Key::Space));
+
+    events::keyboard::send(&EventType::KeyPress(Key::KeyS));
+    events::keyboard::send(&EventType::KeyRelease(Key::KeyS));
+    events::keyboard::send(&EventType::KeyPress(Key::Space));
+    events::keyboard::send(&EventType::KeyRelease(Key::Space));
+
 }
